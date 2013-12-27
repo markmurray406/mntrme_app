6286694e0cd8b0b1dfbcd98ec3a26daa914747eb
Mark <your.email@example.com>
2013-12-27 17:34:18 +0000

    Changed config.assets.compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ MntrmeApp::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Do not fallback to assets pipeline if a precompiled asset is missed. Changed to true 27-Dec-13 as page couldn't show on Heroku
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
