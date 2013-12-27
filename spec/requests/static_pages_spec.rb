145892d311146b430d241ed846df1850cab280de
Mark <your.email@example.com>
2013-12-27 22:03:56 +0000

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    #This tells the tester what we are testing for
+    it "should have the content 'Mntr Me'" do
+    	#This tells the test (Capybara) to visit the home page in the static_pags folder
+      visit '/static_pages/home'
+      #This is the test (Capybara) request
+      expect(page).to have_content('Mntr Me')
+    end
+  end
+
+  describe "Help page" do
+
+    #This tells the tester what we are testing for
+    it "should have the content 'Help'" do
+    	#This tells the test (Capybara) to visit the help page in the static_pags folder
+      visit '/static_pages/help'
+      #This is the test (Capybara) request
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+    
+    #This tells the tester what we are testing for
+    it "should have the content 'About Us'" do
+    	#This tells the test (Capybara) to visit the about page in the static_pags folder
+      visit '/static_pages/about'
+      #This is the test (Capybara) request
+      expect(page).to have_content('About Us')
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #Added Capybara DSL to the RSPec Helper, see Listing 3.10
+  config.include Capybara::DSL
+
 end
