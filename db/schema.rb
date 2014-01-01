6027594ac09843a02a95df8afba42eb22d6c9be1
Mark <your.email@example.com>
2014-01-01 19:36:28 +0000

    Added Integration tests for Links

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131230223203) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "articles", force: true do |t|
-    t.string   "name"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,37 +3,62 @@ require 'spec_helper'
 describe "Static pages" do
 
 subject { page }
+  # See Listing 5.39
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+  end
+
+
 describe "Home page" do
   #This tells the test (Capybara) to visit the home page as described in the routes.rb file
    before { visit root_path }
 
-  #Test 1: Mntr Me is mentioned on the page.
-  it { should have_content('Mntr Me') }
+  #Test 1: Mntr Me is mentioned on the page, # See Listing 5.39
+  let(:heading)    { 'Mntr Me' }
+  it_should_behave_like "all static pages"
+
   #Test 2: Mntr Me shows in the tab of a browser.
   it { should have_title("Mntr Me") }
+
+  #Test 3: This tests the Links to make sure it goes to the correct place.
+  # http://stackoverflow.com/questions/11916962/how-to-keep-rspec-tests-dry-with-lots-of-have-link
+  it { should have_link('Sign up now!', href: usersignup_path) }
+  
+  #Header - Tests 4 and 5.
+  it { should have_link('Home', href: root_path) }
+  it { should have_link('Help', href: help_path) }
+
+  #Footer - Test 6 and 7.
+  it { should have_link('About', href: about_path) }
+  it { should have_link('Contact', href: contact_path) }
 end
 
 describe "Help page" do
   #This tells the test (Capybara) to visit the help page as described in the routes.rb file
    before { visit help_path }
 
-  #Test 3: Help is mentioned on the page.
-  it { should have_content('Help') }
+  #Test 8: Help is mentioned on the page.
+  let(:heading)    { 'Help' }
+  it_should_behave_like "all static pages"
 end
 
 describe "About page" do
   #This tells the test (Capybara) to visit the about page as described in the routes.rb file
    before { visit about_path }
 
-  #Test 4: About is mentioned on the page.
-  it { should have_content('About') }
+  #Test 9: About is mentioned on the page.
+  let(:heading)    { 'About' }
+  it_should_behave_like "all static pages"
 end
 
   describe "Contact page" do
   #This tells the test (Capybara) to visit the contact page as described in the routes.rb file
    before { visit contact_path }
 
-  #Test 5: Contact is mentioned on the page.
-  it { should have_content('Contact') }
+  #Test 10: Contact is mentioned on the page.
+  #it { should have_content('Contact') }
+  let(:heading)    { 'Contact' }
+  it_should_behave_like "all static pages"
   end
+
 end
